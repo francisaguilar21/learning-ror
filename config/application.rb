ebb20b8d4c96fcd1a56a47bfc0c8a22f6af08155
Francis Aguilar <francis.aguilar11@gmail.com>
2024-12-20 17:22:19 +0800

    Fix lint error by rubocop

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Store
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.autoload_paths += %W(#{config.root}/app/services)
+    config.autoload_paths += %W[#{config.root}/app/services]
   end
 end
